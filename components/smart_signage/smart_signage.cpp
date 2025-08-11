e927673dedae307defd09442fe58d2a44365d7d8
rahuljeyaraj <rahuljeyaraj@gmail.com>
2025-08-11 06:46:26 +0530

    listing all files and playing audio from one

--- components/smart_signage/smart_signage.cpp
+++ components/smart_signage/smart_signage.cpp
@@ -1,4 +1,13 @@
-// --- at file top ---
+// keep your existing includes…
+#include "smart_signage.h"
+#include "log.h"
+#include "board.h"
+
+// NEW: bare Arduino + IDF I2S std API (no AudioTools)
+#include <Arduino.h>
+#include "driver/i2s_std.h"
+#include <math.h>
+
 #include "smart_signage.h"
 #include "log.h"
 #include "board.h"
@@ -73,6 +82,9 @@ void print_partition_table() {
     }
     esp_partition_iterator_release(it);
 }
+
+
+
 void SmartSignage::setup() {
     SS_LOGI("SmartSignage setup");
 
@@ -83,6 +95,8 @@ void SmartSignage::setup() {
         SS_LOGE("LittleFS mount failed");
         return;
     }
+
+    list_dir_(LittleFS, "/");
 
     // 2) I2S out
     g_cfg          = g_i2s.defaultConfig(audio_tools::TX_MODE);
@@ -99,7 +113,7 @@ void SmartSignage::setup() {
     g_vol.begin();
 
     // 4) open and start
-    g_file = LittleFS.open("/test.mp3", "r");
+    g_file = LittleFS.open("/warning_1.mp3", "r");
     if (!g_file) {
         SS_LOGE("open /test.mp3 failed");
         return;
@@ -127,71 +141,73 @@ void SmartSignage::loop() {
 void SmartSignage::dump_config() { SS_LOGI("SmartSignage ready"); }
 } // namespace esphome::smart_signage
 
-// #include "smart_signage.h"
-// #include "log.h"
-// #include "board.h"
-// #include <Arduino.h>
-// #include <Wire.h>
-// #
+// // #include "smart_signage.h"
+// // #include "log.h"
+// // #include "board.h"
+// // #include <Arduino.h>
+// // #include <Wire.h>
+// // #
 
-// namespace esphome::smart_signage {
+// // namespace esphome::smart_signage {
 
-// // Controller controller(logger);
-// // SmartSignageApp app(controller, logger);
+// // // Controller controller(logger);
+// // // SmartSignageApp app(controller, logger);
 
-// // // Radar setup
-// // HardwareSerial radarSerial(1);
-// // LD2410RadarHal radarHal(radarSerial, RADAR_RX_PIN, RADAR_TX_PIN, logger);
-// // RadarDriver frontRadar(Id::FrontRadar, controller, radarHal, logger);
-// // IMU setup
-// // ESP32-S3: supports two I2C peripherals (0/1 = Wire/Wire1).
-// // ESP32-C3: only one I2C peripheral (0 = Wire).
-// // MPU6500   imu(Wire);
-// // I2cImuHal imuHal(imu, Wire, MPU6500_DEFAULT_ADDRESS, I2C_SDA_PIN, I2C_SCL_PIN);
-// // ImuDriver imuDrver(Id::FallDetector, controller, imuHal);
-// // // LED setup
-// // EspLedHal ledHal(LED0_PIN);
-// // LedDriver frontLed(Id::FrontLed, controller, ledHal, logger);
+// // // // Radar setup
+// // // HardwareSerial radarSerial(1);
+// // // LD2410RadarHal radarHal(radarSerial, RADAR_RX_PIN, RADAR_TX_PIN, logger);
+// // // RadarDriver frontRadar(Id::FrontRadar, controller, radarHal, logger);
+// // // IMU setup
+// // // ESP32-S3: supports two I2C peripherals (0/1 = Wire/Wire1).
+// // // ESP32-C3: only one I2C peripheral (0 = Wire).
+// // // MPU6500   imu(Wire);
+// // // I2cImuHal imuHal(imu, Wire, MPU6500_DEFAULT_ADDRESS, I2C_SDA_PIN, I2C_SCL_PIN);
+// // // ImuDriver imuDrver(Id::FallDetector, controller, imuHal);
+// // // // LED setup
+// // // EspLedHal ledHal(LED0_PIN);
+// // // LedDriver frontLed(Id::FrontLed, controller, ledHal, logger);
 
-// // // Speaker setup
-// // audio_tools::AudioSourceLittleFS source("/", ".mp3");
-// // audio_tools::MP3DecoderHelix decoder;
-// // I2SAudioHal audioHal(source, decoder, I2S_BCLK_PIN, I2S_LRCK_PIN, I2S_DATA_PIN);
-// // AudioDriver audioDriver(Id::FrontSpeaker, controller, audioHal, logger);
+// // // // Speaker setup
+// // // audio_tools::AudioSourceLittleFS source("/", ".mp3");
+// // // audio_tools::MP3DecoderHelix decoder;
+// // // I2SAudioHal audioHal(source, decoder, I2S_BCLK_PIN, I2S_LRCK_PIN, I2S_DATA_PIN);
+// // // AudioDriver audioDriver(Id::FrontSpeaker, controller, audioHal, logger);
 
-// SmartSignage::SmartSignage(const UiHandles &ui, const char *configJson)
-//     : nvsConfigManager_{kNVSNamespace}, userIntf_{nvsConfigManager_, ui, configJson}, //
-//                                                                                       //
-//       ctrlQ_{}, radarQ_{}, imuQ_{}, ledQ_{}, audioQ_{},                               //
-//       //
+// // SmartSignage::SmartSignage(const UiHandles &ui, const char *configJson)
+// //     : nvsConfigManager_{kNVSNamespace}, userIntf_{nvsConfigManager_, ui, configJson}, //
+// //                                                                                       //
+// //       ctrlQ_{}, radarQ_{}, imuQ_{}, ledQ_{}, audioQ_{},                               //
+// //       //
 
-//       radarSerial_{1}, radarHal_{radarSerial_, RADAR_RX_PIN, RADAR_TX_PIN}, radarTimer_{},
-//       radarAo_{radarQ_, ctrlQ_, radarHal_, radarTimer_, "radarTask", 8192, tskIDLE_PRIORITY + 2,
-//       1},
-//       //
-//       imu_{Wire}, imuTimer_{},
-//       imuHal_{imu_, Wire, MPU6500_DEFAULT_ADDRESS, I2C_SDA_PIN, I2C_SCL_PIN},
-//       imuAo_{imuQ_, ctrlQ_, imuHal_, imuTimer_, "imuTask", 8192, tskIDLE_PRIORITY + 2, 1}, //
+// //       radarSerial_{1}, radarHal_{radarSerial_, RADAR_RX_PIN, RADAR_TX_PIN}, radarTimer_{},
+// //       radarAo_{radarQ_, ctrlQ_, radarHal_, radarTimer_, "radarTask", 8192, tskIDLE_PRIORITY +
+// 2,
+// //       1},
+// //       //
+// //       imu_{Wire}, imuTimer_{},
+// //       imuHal_{imu_, Wire, MPU6500_DEFAULT_ADDRESS, I2C_SDA_PIN, I2C_SCL_PIN},
+// //       imuAo_{imuQ_, ctrlQ_, imuHal_, imuTimer_, "imuTask", 8192, tskIDLE_PRIORITY + 2, 1}, //
 
-//       //
-//       ledHal_{LED0_PIN}, ledTimer_{},
-//       ledAo_{ledQ_, ctrlQ_, ledHal_, ledTimer_, "ledTask", 8192, tskIDLE_PRIORITY + 2, 1}, //
+// //       //
+// //       ledHal_{LED0_PIN}, ledTimer_{},
+// //       ledAo_{ledQ_, ctrlQ_, ledHal_, ledTimer_, "ledTask", 8192, tskIDLE_PRIORITY + 2, 1}, //
 
-//       //
-//       ctrlFsm_{radarQ_, imuQ_, ledQ_, audioQ_}, audioFsm_{ctrlQ_}, //
-//       ctrlFsmLogger_{"ctrlFsmLogger"}, audioFsmLogger_{"audioFsmLogger"},
-//       ctrlAo_{ctrlQ_, ctrlFsm_, ctrlFsmLogger_, "ctrlTask", 8192, tskIDLE_PRIORITY + 2, 1},
+// //       //
+// //       ctrlFsm_{radarQ_, imuQ_, ledQ_, audioQ_}, audioFsm_{ctrlQ_}, //
+// //       ctrlFsmLogger_{"ctrlFsmLogger"}, audioFsmLogger_{"audioFsmLogger"},
+// //       ctrlAo_{ctrlQ_, ctrlFsm_, ctrlFsmLogger_, "ctrlTask", 8192, tskIDLE_PRIORITY + 2, 1},
 
-//       audioAo_{audioQ_, audioFsm_, audioFsmLogger_, "audioTask", 8192, tskIDLE_PRIORITY + 2, 1}
-//       {}
+// //       audioAo_{audioQ_, audioFsm_, audioFsmLogger_, "audioTask", 8192, tskIDLE_PRIORITY + 2,
+// 1}
+// //       {}
 
-// void SmartSignage::setup() {
-//     SS_LOGI("SmartSignage setup");
-//     LittleFS.begin();
-//     userIntf_.setup();
-//     ctrlQ_.post(ctrl::CmdSetup{});
-// }
-// void SmartSignage::loop() {}
-// void SmartSignage::dump_config() { SS_LOGI("SmartSignage ready"); }
+// // void SmartSignage::setup() {
+// //     SS_LOGI("SmartSignage setup");
+// //     LittleFS.begin();
+// //     userIntf_.setup();
+// //     ctrlQ_.post(ctrl::CmdSetup{});
+// // }
+// // void SmartSignage::loop() {}
+// // void SmartSignage::dump_config() { SS_LOGI("SmartSignage ready"); }
 
-// } // namespace esphome::smart_signage
+// // } // namespace esphome::smart_signage
