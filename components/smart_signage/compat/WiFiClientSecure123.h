db7e2be5af1ea1a205c1afef0bbacaa7897fb7fb
rahuljeyaraj <rahuljeyaraj@gmail.com>
2025-08-11 10:56:30 +0530

    removing WiFiClientSecure.h unsucessfull fix

--- components/smart_signage/compat/WiFiClientSecure123.h
+++
@@ -1,8 +0,0 @@
-// #pragma once
-// #if __has_include(<NetworkClientSecure.h>)
-// // Arduino-ESP32 3.x renamed the header
-// #include <NetworkClientSecure.h>
-// #else
-// // Arduino-ESP32 2.x: use the framework's original header
-// #include_next <WiFiClientSecure.h>
-// #endif
